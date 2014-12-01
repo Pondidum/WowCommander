c059c900140669a740a7344c5e32d6cd6c2d45c7
andy <andy@andydote.co.uk>
2014-12-01 17:54:45 +0000

    missions: split list and item to separate files

---
+++ components/missionItem.jsx
@@ -0,0 +1,9 @@
+var MissionItem = React.createClass({
+	render: function() {
+		return (
+			<div>
+				{this.props.data.name}
+			</div>
+		);
+	}
+});
--- components/missionList.jsx
+++ components/missionList.jsx
@@ -1,9 +1,39 @@
-/** @jsx React.DOM */
-
 var MissionList = React.createClass({
+	
+	loadFromServer: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			success: function(data) {
+				this.setState({missions: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+
+	getInitialState: function() {
+		return { missions: [] };
+	},
+
+	componentDidMount: function() {
+		this.loadFromServer();
+		setInterval(this.loadFromServer, this.props.interval);
+	},
+
 	render: function() {
+
+		var missions = this.state.missions.map(function(mission) {
+			return (
+				<MissionItem key={mission.id} data={mission} />
+			);
+		});
+
 		return (
-			<h1>Testing</h1>
+			<div className="missionList">
+				{missions}
+			</div>
 		);
 	}
 });
